805afe4e5ad30b806182740aec086e9b12ec0ba5
Simon Kafader <simon.kafader@students.unibe.ch>
2016-03-06 22:04:15 +0100

    added SwapSquare

--- exercise_02/src/snakes/SwapSquare.java
+++ exercise_02/src/snakes/SwapSquare.java
@@ -5,4 +5,21 @@ public class SwapSquare extends Square {
 	public SwapSquare(Game game, int position) {
 		super(game, position);
 	}
+
+	protected ISquare destination(){
+		return game.getSquare(game.currentPlayer().position()); //currentPlayer() is acutally the next player
+	}
+
+	@Override
+	public ISquare landHereOrGoHome(){
+		if(!this.isOccupied()){
+			ISquare destination = destination();
+			destination.leave(game.currentPlayer());
+			this.enter(game.currentPlayer());
+			return destination;
+		}
+		else{
+			return game.firstSquare();
+		}
+	}
 }
