a1192554678bcdd397be50d68d969bc38e6f9f6e
Simon Kafader <simon.kafader@students.unibe.ch>
2016-03-08 15:44:38 +0100

    Added the Wormholes

--- exercise_02/src/snakes/WormholeExit.java
+++ exercise_02/src/snakes/WormholeExit.java
@@ -1,9 +1,18 @@
 package snakes;
 
+import java.util.ArrayList;
+
 public class WormholeExit extends Square {
+
+	private static ArrayList<WormholeExit> Exits;
 
 	public WormholeExit(Game game, int position) {
 		super(game, position);
+		Exits.add(this);
+	}
+
+	public static ArrayList getExits(){
+		return Exits;
 	}
 
 }
