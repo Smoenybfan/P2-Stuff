ad1d666d0fb31ecad7720071764a22357ea5847f
Simon Kafader <simon.kafader@students.unibe.ch>
2016-03-13 23:43:21 +0100

    Added move methods, need corrections

--- exercise_03/src/turtle/BoardMaker.java
+++ exercise_03/src/turtle/BoardMaker.java
@@ -7,6 +7,7 @@ public class BoardMaker {
 	private final static int SIZE = 100;
 	private int xPosition;
 	private int yPosition;
+	private String[] commands;
 
 	/**
 	 * Parse the given turtle program and evaluate it. Render the trail as
@@ -41,17 +42,18 @@ public class BoardMaker {
 		}
 		*/
 
-		CommandParser parser = new CommandParser(turtleProgram);
+		CommandParser parser = new CommandParser(turtleProgram, xPosition, yPosition, board);
 		try {
-			parser.parse();
+			commands= parser.parse();
+			return move();
 		}
 
 		catch (Exception err){
-
+			throw new ParserException();
 		}
 
 		// TODO: remove this and return a board instead
-		return board;
+
 		//throw new UnsupportedOperationException();
 	}
 
@@ -64,6 +66,82 @@ public class BoardMaker {
 		board= new boolean[SIZE][SIZE];
 		xPosition = SIZE/2;
 		yPosition = SIZE/2;
+		board[xPosition][yPosition]=true;
 		return board;
+	}
+
+	public boolean[][] move(){
+
+		switch (commands[0]){
+			case "right":   moveRight();
+				break;
+			case "left":    moveLeft();
+				break;
+			case "down":    moveDown();
+				break;
+			case "up":      moveUp();
+				break;
+			case "jump":    moveJump();
+				break;
+		}
+
+		return board;
+	}
+
+	/**
+	 * Helper Method that marks the way of the turtle and moves it up
+	 */
+	private void moveUp() {
+		assert commands.length == 2;
+		for (int i = yPosition; i < Integer.parseInt( commands[1]); i++) {
+			board[xPosition][i] = true;
+		}
+		yPosition+=Integer.parseInt( commands[1]);
+	}
+
+	/**
+	 * Helper method that marks the way of the turtle and moves it right
+	 */
+	private void moveRight() {
+		assert  commands.length == 2;
+		for (int i = 1; i <= Integer.parseInt( commands[1]); i++) {
+			board[xPosition+i][yPosition] = true;
+		}
+		xPosition+=Integer.parseInt( commands[1]);
+	}
+
+	/**
+	 * Helper method that marks the way of the turtle and moves it down
+	 */
+
+	private void moveDown() {
+		assert  commands.length == 2;
+		for (int i = xPosition; i < Integer.parseInt( commands[1]); i--) {
+			board[i][yPosition] = true;
+		}
+		yPosition-=Integer.parseInt( commands[1]);
+	}
+
+	/**
+	 * Helper method that marks the way of the turtle and moves it left
+	 */
+
+	private void moveLeft() {
+		assert  commands.length == 2;
+		for (int i = xPosition; i < Integer.parseInt( commands[1]); i--) {
+			board[i][yPosition] = true;
+		}
+		xPosition -= Integer.parseInt( commands[1]);
+	}
+
+	/**
+	 * Helper method that jumps the turtle and marks the new position.
+	 */
+
+	private void moveJump(){
+		assert  commands.length==3;
+		board[Integer.parseInt( commands[1])][Integer.parseInt( commands[2])]=true;
+		xPosition=Integer.parseInt( commands[1]);
+		yPosition=Integer.parseInt( commands[2]);
 	}
 }
--- exercise_03/src/turtle/CommandParser.java
+++ exercise_03/src/turtle/CommandParser.java
@@ -1,5 +1,6 @@
 package turtle;
 
+
 /**
  * This class checks if the entered command is valid.
  * If the command is valid, it moves the turtle to the correct place and colors the path.
@@ -9,82 +10,94 @@ package turtle;
 public class CommandParser {
 
     private String turtleProgram;
+    private int xPosition;
+    private int yPosition;
+    private boolean[][] board;
 
-    public CommandParser(String turtleProgram){
-        this.turtleProgram=turtleProgram;
+    public CommandParser(String turtleProgram, int xPosition, int yPosition, boolean[][] board) {
+        this.turtleProgram = turtleProgram;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.board = board;
     }
 
     /**
-     *
      * @return the board with the correct move
      */
-    public boolean[][] parse() {
-        assert turtleProgram!= null;
+    public String[] parse() {
+        assert turtleProgram != null;
         assert invariant();
-        String[] str = turtleProgram.split("\\s+");
 
-        //TODO: write a correct return
-        // return some board ;
+        return turtleProgram.split("\\s+");
     }
 
     /**
      * An invariant method for the class CommandParser
+     *
      * @return true if the turtleProgram is a valid command
      */
 
-    private boolean invariant(){
+    private boolean invariant() {
 
-        return (parseRight() || parseLeft() || parseDown() || parseUp() ||parseJump());
+        return (parseRight() || parseLeft() || parseDown() || parseUp() || parseJump());
     }
+
 
     /**
      * Helper method that checks if the entered command is a valid "right" command
+     *
      * @return true if the turtleProgram is a valid "right" command
      */
 
-    private boolean parseRight(){
+    private boolean parseRight() {
         String[] str = turtleProgram.split("\\s+");
-        return (str[0]=="right" && 0<=Integer.parseInt(str[1]) && Integer.parseInt(str[1])<=100);
+        return (str[0] == "right" && 0 <= Integer.parseInt(str[1]) && Integer.parseInt(str[1]) <= 100);
     }
 
     /**
      * Helper method that checks if the entered command is a valid "left" command
+     *
      * @return true if the turtleProgram is a valid "left" command
      */
 
-    private boolean parseLeft(){
+    private boolean parseLeft() {
         String[] str = turtleProgram.split("\\s+");
-        return (str[0]=="left" && 0<=Integer.parseInt(str[1]) && Integer.parseInt(str[1])<=100);
+        return (str[0] == "left" && 0 <= Integer.parseInt(str[1]) && Integer.parseInt(str[1]) <= 100);
     }
 
     /**
      * Helper method that checks if the entered command is a valid "up" command
+     *
      * @return true if the turtleProgram is a valid "up" command
      */
 
-    private boolean parseUp(){
+    private boolean parseUp() {
         String[] str = turtleProgram.split("\\s+");
-        return (str[0]=="up" && 0<=Integer.parseInt(str[1]) && Integer.parseInt(str[1])<=100);
+        return (str[0] == "up" && 0 <= Integer.parseInt(str[1]) && Integer.parseInt(str[1]) <= 100);
     }
 
     /**
      * Helper method that checks if the entered command is a valid "down" command
+     *
      * @return true if the turtleProgram is a valid "down" command
      */
 
-    private boolean parseDown(){
+    private boolean parseDown() {
         String[] str = turtleProgram.split("\\s+");
-        return (str[0]=="down" && 0<=Integer.parseInt(str[1]) && Integer.parseInt(str[1])<=100);
+        return (str[0] == "down" && 0 <= Integer.parseInt(str[1]) && Integer.parseInt(str[1]) <= 100);
     }
 
     /**
      * Helper method that checks if the entered command is a valid "jump" command
+     *
      * @return true if the turtleProgram is a valid "jump" command
      */
 
-    private boolean parseJump(){
+    private boolean parseJump() {
         String[] str = turtleProgram.split("\\s+");
-        return (str[0]=="jump" && 0<=Integer.parseInt(str[1]) && Integer.parseInt(str[1])<=100 && 0<=Integer.parseInt(str[2]) && Integer.parseInt(str[2])<=100);
+        return (str[0] == "jump" && 0 <= Integer.parseInt(str[1]) && Integer.parseInt(str[1]) <= 100 && 0 <= Integer.parseInt(str[2]) && Integer.parseInt(str[2]) <= 100);
 
     }
+
+
 }
