78fa1478e6ac2c5ff30ebf87de32fb1fcb5e8a65
kafader <kafader>
2016-04-10 14:43:21 +0200

    added tests for snakes

---
+++ exercise_03/src/turtle/CommandParserTest.java
@@ -0,0 +1,22 @@
+package turtle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kafader on 07.04.2016.
+ */
+public class CommandParserTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void parse() throws Exception {
+
+    }
+
+}
